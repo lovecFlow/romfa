068dc0af84e17e1622c392813f8861d2a291a4a7
Nikita <nikita.kopytov00@gmail.com>
2022-03-29 19:41:10 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// alert('hello');
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = +prompt("Are u 18?", ""); 
+// console.log(answer + 5); 
+
+const answers = []; 
+
+// answers[0] = prompt('Kak vashe imya', ''); 
+// answers[1] = prompt('Kak vaha familya', ''); 
+// answers[2] = prompt('Skolko vam let', ''); 
+
+// console.log(typeof(answers)); 
+// console.log(typeof(null)); 
+
+// Интерполяция
+
+// const category = 'toys'; 
+
+// console.log(`https://somerurl.com/${category}/5`); 
+
+// const user = "Ivan"; 
+
+// alert(`Privet, ${user}`);
+
+// console.log('arr' + "- object"); 
+// console.log(4 + +"5"); 
+
+// let incr = 10, 
+//     decr = 10; 
+
+// incr++; 
+// decr--;
+
+// console.log(incr); 
+// console.log(decr); 
+
+// console.log(5%2);
+
+// console.log(2*4 == 8);
+
+const isCheked = true,
+      isClose = true; 
+
+console.log(isCheked && isClose);
